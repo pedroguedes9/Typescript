54f5d222932435aef1bd117857bc36b21ea6e65d
Pedro Chaves Guedes <pedro.guedes.mcz@gmail.com>
2025-07-27 10:58:59 -0300

    Update script.ts

--- src/script.ts
+++ src/script.ts
@@ -1,14 +1,2 @@
-function resumo (
-    usuario: {nome:string, idade:number}, 
-    escolaridade: {nivel:string}
-) { // pode separar por ;
-    return `Olá ${usuario.nome}, você tem ${usuario.idade}, e está no nível ${escolaridade.nivel}`
-}
-let user = {
-    nome: "pedro",
-    idade: 90
-}
-let estudos = {
-    nivel: "superior"
-}
-console.log(resumo(user, estudos))
+let idade:string|number = 17
+idade = "15"
